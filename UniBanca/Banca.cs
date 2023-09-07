3a5862cbab598ef4bdd683a1ec827599c7a4124a
Giuseppe Cifarelli <cifarelli1995@gmail.com>
2023-09-07 06:11:03 +0200

    End

--- UniBanca/Banca.cs
+++ UniBanca/Banca.cs
@@ -10,7 +10,7 @@ namespace UniBanca
     internal class Banca
     {
         public static List<ContoCorrente> Conti = new List<ContoCorrente>();
-        public static int nrConti = 5;
+        public static List<Movimento> Movimenti = new List<Movimento>();
        
         public static void CreaConti()
         {
@@ -30,7 +30,7 @@ namespace UniBanca
         {
             Console.WriteLine("===============MENU===============");
             Console.WriteLine("1. Crea nuovo conto");
-            Console.WriteLine("2. Movimento sul conto");
+            Console.WriteLine("2. Operazione sul conto");
             Console.WriteLine("3. Lista dei movimenti");
             Console.WriteLine("4. Saldo dei conti memorizzati");
             Console.WriteLine("5. ESCI");
@@ -46,8 +46,18 @@ namespace UniBanca
                         Menu();
                         break;
 
+                    case 2:
+                        MenuMovimento();   
+                        Menu();
+                        break;
+
                     case 3:
                         MostraListaMovimenti();
+                        Menu();
+                        break;
+
+                    case 4:
+                        MostraSaldoConti();
                         Menu();
                         break;
 
@@ -75,21 +85,126 @@ namespace UniBanca
             Console.WriteLine("Inserisci il cognome del proprietario");
             string cognome = Console.ReadLine();
             ContoCorrente conto = new ContoCorrente(nome, cognome);
-            nrConti += 1;
-            conto.NrConto = nrConti;
+            conto.NrConto = Conti.Count + 1;
             Conti.Add(conto);
+        }
+
+        public static void MenuMovimento()
+        {
+            Console.WriteLine("Quale operazione vuoi effettuare?");
+            Console.WriteLine("1. ACCREDITO");
+            Console.WriteLine("2. ADDEBITO");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Accredito();
+                    break;
+
+                case 2:
+                    Addebito();
+                    break;
+
+                default:
+                    Console.WriteLine("L' operazione inserita non esiste!");                   
+                    break;
+            }
+        }
+
+        public static void Accredito()
+        {
+            Console.WriteLine($"Sono presenti {Conti.Count} conti memorizzati");
+            Console.WriteLine("Su quale conto vuoi effettuare l' accredito?");
+            int nrConto = Convert.ToInt16(Console.ReadLine());
+            if (nrConto > Conti.Count)
+            {
+                Console.WriteLine("Il Numero di Conto inserito non esiste");
+                Menu();
+            }
+            Console.WriteLine("Quanto vuoi versare sul conto?");
+            double accredito = Convert.ToDouble(Console.ReadLine());
+            foreach (ContoCorrente conto in Conti)
+            {
+                if (conto.NrConto == nrConto)
+                {
+                    conto.Saldo += accredito;
+                    Movimento movimento = new Movimento(accredito);
+                    movimento.Tipo = "Accredito";
+                    movimento.NrConto = nrConto;
+                    Movimenti.Add(movimento);
+                    Console.WriteLine("Versamento riuscito!");
+                }
+            }
+        }
+
+        public static void Addebito()
+        {
+            Console.WriteLine($"Sono presenti {Conti.Count} conti memorizzati");
+            Console.WriteLine("Su quale conto vuoi effettuare l' addebito?");
+            int nrConto = Convert.ToInt16(Console.ReadLine());
+            if (nrConto > Conti.Count)
+            {
+                Console.WriteLine("Il Numero di Conto inserito non esiste");
+                Menu();
+            }
+            Console.WriteLine("Quanto vuoi addebitare sul conto?");
+            double addebito = Convert.ToDouble(Console.ReadLine());
+            foreach (ContoCorrente conto in Conti)
+            {
+                if (conto.NrConto == nrConto)
+                {
+                    if(addebito > conto.Saldo) {
+                        Console.WriteLine("Il saldo disponibile è insufficiente");
+                        Menu();
+                    }
+                    else { 
+                        conto.Saldo -= addebito;
+                        Movimento movimento = new Movimento(addebito);
+                        movimento.Tipo = "Addebito";
+                        movimento.NrConto = nrConto;
+                        Movimenti.Add(movimento);
+                        Console.WriteLine("Addebito riuscito!");
+                    }
+                }
+            }
+
         }
 
         public static void MostraListaMovimenti()
         {
-            Console.WriteLine("Qual è il numero di Conto che vuoi vedere?");
+            Console.WriteLine($"Sono presenti {Conti.Count} conti memorizzati");
+            Console.WriteLine("Qual è il numero di Conto che vuoi controllare?");
             int option = Convert.ToInt16(Console.ReadLine());
+            if (option > Conti.Count)
+            {
+                Console.WriteLine("Il Numero di Conto inserito non esiste");
+                Menu();
+            }
+
             foreach (ContoCorrente conto in Conti)
             {
-                if(conto.NrConto == option)
+                if (conto.NrConto == option)
                 {
-                    Console.WriteLine($"Il conto di {conto.NomeCorrentista} {conto.CognomeCorrentista} aperto in data {conto.DataApertura} ha un saldo di {conto.Saldo}");
+                    Console.WriteLine($"Il conto di {conto.NomeCorrentista} {conto.CognomeCorrentista} aperto in data {conto.DataApertura} ha un saldo di ${conto.Saldo}");
                 }
+            }
+
+            Console.WriteLine("=====APERTURA LISTA MOVIMENTI=====");
+            foreach (Movimento mov in Movimenti)
+            {
+                if(mov.NrConto == option)
+                {
+                    Console.WriteLine($"E' stato effettuato un {mov.Tipo} di ${mov.Importo}");
+                } 
+            }
+            Console.WriteLine("=====CHIUSURA LISTA MOVIMENTI=====");
+        }
+
+        public static void MostraSaldoConti()
+        {
+            foreach(ContoCorrente conto in Conti)
+            {
+                Console.WriteLine($"Il conto di {conto.NomeCorrentista} {conto.CognomeCorrentista} aperto in data {conto.DataApertura} ha un saldo di ${conto.Saldo}");
             }
         }
     }
---
+++ UniBanca/Movimento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniBanca
+{
+    internal class Movimento
+    {
+        public string Tipo;
+
+        public double Importo { get; set; }
+
+        public int NrConto;
+
+        public Movimento() { }
+
+        public Movimento(double importo)
+        {
+            Importo = importo;
+        }
+    }
+}
